a69933f7528e3d604c1a1c898e62b68c9c8b485c
Łukasz Józwiak <ydk2@ymail.com>
2016-11-17 15:11:48 +0100

    start doc

--- libriares/core/fileutils.php
+++ libriares/core/fileutils.php
@@ -1,10 +1,31 @@
 <?php
-
-
-
 /**
-* Class FileUtils
-*/
+* 
+ * PHPRender fast and simple to use PHP MVC framework
+ *
+ * MVC Framework for PHP 5.2 + with PHP files views part of YMVC System
+ * Router to dynamic loading controllers from GET
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Framework, MVC
+ * @package    YMVC System
+ * @subpackage FileUtils
+ * @author     ydk2 <me@ydk2.tk>
+ * @copyright  1997-2016 ydk2.tk
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    2.0.1
+ * @link       http://ymvc.ydk2.tk
+ * @see        YMVC System
+ * @since      File available since Release 2.0.0
+ 
+ */
 class FileUtils {
 	public static function Download($file) {
 		//$		file = ($file==NULL)?$_GET['pobierz']:$file;
